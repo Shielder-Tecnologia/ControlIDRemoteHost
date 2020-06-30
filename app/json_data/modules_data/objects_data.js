f8d2d5bf80b682a398313ffd66ea70ba617d3ef2
vinicortizo <55987525+vinicortizo@users.noreply.github.com>
2020-06-30 12:16:48 -0300

    Update objects_data.js
    Adicionando JSON dos objetos

--- app/json_data/modules_data/objects_data.js
+++ app/json_data/modules_data/objects_data.js
@@ -1,10 +1,49 @@
-
+var create_users = {
+	"object": "users",
+	"values": [{}]
+}
 
 var load_users = {
 	"object": "users"
 }
+var delete_user = {
+	"object": "users"
+}
+
+
+var create_templates = {
+	"object": "templates",
+	"values": [{}]
+}
+
+var load_templates = { 
+	"object": "templates"
+}
+
+var delete_templates = {
+	"object": "templates"
+}
+
+
+var create_cards = {
+	"object": "cards",
+	"values": [{}]
+}
+
+var load_cards = { 
+	"object": "cards"
+}
+
+var delete_card = {
+	"object": "cards"
+}
+
+
 objects_data = {
-    load_users : load_users
+    load_users : load_users,
+    create_templates : create_templates,
+    load_templates : load_templates,
+    delete_templates : delete_templates
 }
 module.exports = objects_data
 
