ea7cb6813d7c2f23c814e395d3d6d685db468122
vinicortizo <55987525+vinicortizo@users.noreply.github.com>
2020-07-06 15:53:48 -0300

    autorizaBox

--- app/Controller/push_Shielder.js
+++ app/Controller/push_Shielder.js
@@ -1,2 +1,12 @@
 var shielderweb = require('./shielder_web');
-
+autorizaBox = function(ip,serialnumber){
+    return new Promise((resolve,reject)=>{
+        response = shielderweb.autorizaBox(ip,serialnumber).then(response=>{
+            resolve(response)
+        });
+        
+    })
+}
+module.exports = {
+    autorizaBox
+}
