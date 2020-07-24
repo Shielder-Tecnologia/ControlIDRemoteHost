f66a5759d2bc1f472e868a6c44078c91b3ff6b15
vinicortizo <55987525+vinicortizo@users.noreply.github.com>
2020-07-24 14:59:23 -0300

    Update index.js

--- app/paths/index.js
+++ app/paths/index.js
@@ -102,7 +102,8 @@ module.exports = ()=>{
                 
                 for (var i=0; i<device_list.length;i++){
                     if(device_list[i] && device_list[i].lastOn !== 'undefined'){
-                        console.log(device_list[i].id +" Last On "+moment().valueOf() - device_list[i].lastOn)
+                        console.log(device_list[i].id +" Last On ")
+                        console.log(moment().valueOf() - device_list[i].lastOn)
                         if(moment().valueOf() - device_list[i].lastOn >300000){
                             device_list.splice(i,device_list.length);
                         }
