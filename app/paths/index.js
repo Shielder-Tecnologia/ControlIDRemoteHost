de2b99e4385ed9f872ece8d1254990b6507acd01
vinicortizo <55987525+vinicortizo@users.noreply.github.com>
2021-01-28 12:44:06 -0300

    numero nao inteiro

--- app/paths/index.js
+++ app/paths/index.js
@@ -141,7 +141,7 @@ module.exports = ()=>{
                 data = device_list.filter(function( element ) {
                     return element !== undefined;
                  });
-                 
+
                 if(device_list)
                     var dIndex = device_list.findIndex(x => x.devid == req.query.deviceId);
                 
@@ -258,20 +258,19 @@ module.exports = ()=>{
 
                             //caso nao tenha sido registrado no shielder ele espera para colocar o id
                             if(!Number.isInteger(device_list[dIndex].id) || device_list[dIndex].id<=4){
-                                if(response.indexOf(';')>=0){
-                                    var id = response.split(";")
-                                    device_list[dIndex].id = id[0];
-                                    device_list[dIndex].timeout = 0;
-                                    control.get_request_set_relay(0,device_list[dIndex].devid,push_list).then(response=>{                                
-                                        push_list = response
-                                    }).catch(error=>{
-                                        console.log(error)
-                                    })
+                                if(!Number.isInteger(response)){
+                                    if(response.indexOf(';')>=0){
+                                        var id = response.split(";")
+                                        device_list[dIndex].id = id[0];
+                                        device_list[dIndex].timeout = 0;
+                                        control.get_request_set_relay(0,device_list[dIndex].devid,push_list).then(response=>{                                
+                                            push_list = response
+                                        }).catch(error=>{
+                                            console.log(error)
+                                        })
+                                    }
                                 }else{
-
-                                    if(Number.isInteger(response))
-                                        device_list[dIndex].id = response;
-                                    
+                                    device_list[dIndex].id = response;
                                 }
                                 req.app.set('device_list',device_list);
                             }
