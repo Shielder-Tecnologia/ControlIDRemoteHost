1359036a47e864cbd50963079316e2310ddcbe54
vinicortizo <55987525+vinicortizo@users.noreply.github.com>
2021-01-29 10:48:16 -0300

    erro

--- app/Controller/controlServer.js
+++ app/Controller/controlServer.js
@@ -789,7 +789,7 @@ let controlCopia = (response,device_list,push_list) =>{
       }else if(response[0].tag && response[0].descricao != "SENHA"){
          //CREATE CARD
          var responseArray = response.split(",")
-         var tag = (Math.(pow(2,32) * responseArray[0]) + responseArray[1])
+         var tag = (Math.pow(2,32) * responseArray[0]) + responseArray[1]
          p.devid = device_list[dIndex].devid;
          p.request = { verb: "POST", endpoint: "create_objects", body: { 
             "object": "cards",
