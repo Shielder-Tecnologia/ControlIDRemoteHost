7c79b2c1056afe420aff0f5e3bf40b1ff761496f
vinicortizo <55987525+vinicortizo@users.noreply.github.com>
2021-05-18 16:53:13 -0300

    Update index.js

--- app/paths/index.js
+++ app/paths/index.js
@@ -508,7 +508,7 @@ module.exports = ()=>{
                 let cardid = req.body.value;
                 //conversao cartao
                 let bfvirgula= (cardid - 45678) / 4294967296;
-                bfvirgula = Math.round(bvirgula);
+                bfvirgula = Math.round(bfvirgula);
                 let num1 = bfvirgula * 4294967296;
                 let afvirgula =  cardid - num1;
 
